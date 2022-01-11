802fb1f4f2b3ee56ae192f88bfd9fe4f8f07f3bd
masatonara83 <masato.nara@rakus-partners.co.jp>
2022-01-11 15:24:47 +0900

    update機能作成

--- src/components/ListEmployeeComponent.jsx
+++ src/components/ListEmployeeComponent.jsx
@@ -9,6 +9,11 @@ class ListEmployeeComponent extends Component {
       employees : []
     }
     this.addEmployee = this.addEmployee.bind(this);
+    this.editEmployee = this.editEmployee.bind(this);
+  }
+
+  editEmployee(id){
+    this.props.history.push(`/update-employee/${id}`)
   }
 
   componentDidMount(){
@@ -47,6 +52,9 @@ class ListEmployeeComponent extends Component {
                     <td> { employee.firstName} </td>
                     <td> { employee.lastName} </td>
                     <td> { employee.emailId} </td>
+                    <td>
+                      <button onClick= {() => this.editEmployee(employee.id)} className="btn btn-info updateBtn">update</button>
+                    </td>
                   </tr>
                 )
               }
